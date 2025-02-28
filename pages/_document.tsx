6c237b1efd6c80a1a40bdbe596bfd78a6d55ab0c
I Kadek Sastrawan <sastrawanikadek@gmail.com>
2025-02-28 18:01:57 +0800

    fix: scrollbar styles not applied correctly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html className='dark text-[12px] sm:text-[16px]'>
+    <Html
+      lang='en'
+      className='dark text-[12px] sm:text-[16px] scrollbar scrollbar-thin scrollbar-track-slate-800 scrollbar-thumb-slate-700 scrollbar-thumb-rounded-full'
+    >
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
@@ -11,7 +14,7 @@ const Document = () => {
           rel='stylesheet'
         />
       </Head>
-      <body className='bg-slate-800 scrollbar scrollbar-thin scrollbar-track-slate-800 scrollbar-thumb-slate-700 scrollbar-thumb-rounded-full'>
+      <body className='bg-slate-800'>
         <Main />
         <NextScript />
       </body>
